cd9964539651d5a778d8637766ff3528a6b55da6
MyGithub7777 <asmygithub@gmail.com>
2024-04-07 09:25:06 +0530

    Added login testcases

--- APAutomation/src/test/java/com/odoo/testlayer/LoginTest.java
+++ APAutomation/src/test/java/com/odoo/testlayer/LoginTest.java
@@ -1,5 +1,50 @@
 package com.odoo.testlayer;
 
-public class LoginTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import com.odoo.pagelayer.Homepage;
+import com.odoo.pagelayer.Loginpage;
+import com.odoo.testbase.TestBase;
+
+public class LoginTest extends TestBase{
+
+	@Test
+	public void verifyLoginWithValidDetails()
+	{
+		String expected_result = "My Portal | testing";
+		Homepage homepage_obj = new Homepage(driver);
+		Loginpage loginpage_obj = new Loginpage(driver);
+		logger.info("Object creation");
+		
+		homepage_obj.clickOnSignInLink();
+		loginpage_obj.enterEmail("test@gmail.com");
+		loginpage_obj.enterPassword("Test@1234");
+		loginpage_obj.clickOnLoginButton();
+		logger.info("login details entered");
+		
+		String actual_result = driver.getTitle();
+		System.out.println(actual_result);
+		
+		Assert.assertEquals(actual_result, expected_result);	
+	}
+	
+	@Test
+	public void verifyLoginWithInValidDetails()
+	{
+		String expected_result = "Wrong login/password";
+		Homepage homepage_obj = new Homepage(driver);
+		Loginpage loginpage_obj = new Loginpage(driver);
+		logger.info("Object creation");
+		homepage_obj.clickOnSignInLink();
+		loginpage_obj.enterEmail("test@gmail.com");
+		loginpage_obj.enterPassword("Test");
+		loginpage_obj.clickOnLoginButton();
+		logger.info("login details entered");
+		
+		String actual_result = loginpage_obj.getErrorMessage();
+		System.out.println(actual_result);
+		
+		Assert.assertEquals(actual_result, expected_result);	
+	}
 }
